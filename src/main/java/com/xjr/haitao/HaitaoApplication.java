0b6988d36a40c5ad19c4079ece9d76ef82a0c00b
Rookie1996 <327474264@qq.com>
2021-02-22 00:09:02 +0800

    初始化haitao后端项目

---
+++ src/main/java/com/xjr/haitao/HaitaoApplication.java
@@ -0,0 +1,13 @@
+package com.xjr.haitao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaitaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaitaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xjr/haitao/HaitaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjr.haitao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaitaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
